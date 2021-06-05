4b6ccf4095055d19cdc2ee018cad527eeebcf79c
Davi Viana <davi.vc@hotmail.com>
2021-06-04 23:34:13 -0300

    shipyard and boats

--- scenes/Ship.gd
+++ scenes/Ship.gd
@@ -2,42 +2,50 @@ extends KinematicBody2D
 
 var movement = Vector2(0,0)
 export var speed = 2
-export var life = 100
+export var resistance = 1
+export var size = 2
 export var navegation_technologies = 1
 export var fire_power = 1
 
+export var life = 100
 var anchorage_areas = []
 var anchored_area = null
 var arriving = false
 var sailing = false
 var timer = false
+var repositioning = false
 
 func _ready():
+	define_sprite()
 	$AnimationPlayer.play("floating")
-	arrive()#teste
+	reposition()#linha de teste
 
 func _physics_process(delta):
 	if arriving:
 		dock()
-	elif sailing:
+	elif sailing or repositioning:
 		if position.x > 1500:
 			sailing = false
 			movement.x = 0
-			anchored_area.monitorable = true
-			arrive()#teste
+			if anchored_area != null:
+				anchored_area.monitorable = true
+			if repositioning:
+				arrive()
+				repositioning = false
 		elif movement.x < speed:
 			movement.x += 0.01
 	
 	move_and_collide(movement)
 	update_animations()
+	
 
 func update_animations():
 	if movement.x < 0:
-		$CollisionShape2D.scale.x = -1
-		$Sprite.scale.x = -1
+		$Area2D2/CollisionPolygon2D.scale.x = -abs($Area2D2/CollisionPolygon2D.scale.x)
+		$Sprite.scale.x = -abs($Sprite.scale.x)
 	elif movement.x > 0:
-		$CollisionShape2D.scale.x = 1
-		$Sprite.scale.x = 1
+		$Area2D2/CollisionPolygon2D.scale.x = abs($Area2D2/CollisionPolygon2D.scale.x)
+		$Sprite.scale.x = abs($Sprite.scale.x)
 
 func sail():
 	sailing = true
@@ -66,13 +74,15 @@ func _on_DetectionDelay_timeout():
 	else:
 		anchorage_areas = []
 		arrive()
+		
+func reposition():
+	repositioning = true
 
 func dock():
 	var distance = position.x - anchored_area.get_children()[0].position.x
 	if position.x <= anchored_area.get_children()[0].position.x:
 		movement.x = 0
 		arriving = false
-		sail()#teste
 	elif movement.x < -0.1 and distance < 500:
 		if speed != 1:
 			if distance > 300:
@@ -83,3 +93,61 @@ func dock():
 			movement.x += 0.001
 	if arriving and movement.x > -0.1:
 		movement.x = -0.1
+
+func define_sprite():
+	if size == 1:
+		if $Area2D2/CollisionPolygon2D.scale.x < 0:
+			$Area2D2/CollisionPolygon2D.scale.x = -0.5
+			$Area2D2/CollisionPolygon2D.scale.y = -1
+		elif $Area2D2/CollisionPolygon2D.scale.x > 0:
+			$Area2D2/CollisionPolygon2D.scale.x = 0.5
+			$Area2D2/CollisionPolygon2D.scale.y = 1
+		
+		if resistance >= 3:
+			if fire_power >=3:
+				$Sprite.texture = load("res://assets/ships/ship18.tres")
+			else:
+				$Sprite.texture = load("res://assets/ships/ship17.tres")
+		else:
+			if fire_power >= 3:
+				$Sprite.texture = load("res://assets/ships/ship16.tres")
+			else:
+				$Sprite.texture = load("res://assets/ships/ship15.tres")
+		
+	elif size == 2:
+		if $Area2D2/CollisionPolygon2D.scale.x < 0:
+			$Area2D2/CollisionPolygon2D.scale.x = -1
+			$Area2D2/CollisionPolygon2D.scale.y = -1
+		elif $Area2D2/CollisionPolygon2D.scale.x > 0:
+			$Area2D2/CollisionPolygon2D.scale.x = 1
+			$Area2D2/CollisionPolygon2D.scale.y = 1
+		
+		if resistance >= 3:
+			if fire_power >=3:
+				$Sprite.texture = load("res://assets/ships/ship28.tres")
+			else:
+				$Sprite.texture = load("res://assets/ships/ship27.tres")
+		else:
+			if fire_power >= 3:
+				$Sprite.texture = load("res://assets/ships/ship26.tres")
+			else:
+				$Sprite.texture = load("res://assets/ships/ship25.tres")
+		
+	elif size == 3:
+		if $Area2D2/CollisionPolygon2D.scale.x < 0:
+			$Area2D2/CollisionPolygon2D.scale.x = -1.2
+			$Area2D2/CollisionPolygon2D.scale.y = -1.2
+		elif $Area2D2/CollisionPolygon2D.scale.x > 0:
+			$Area2D2/CollisionPolygon2D.scale.x = 1.2
+			$Area2D2/CollisionPolygon2D.scale.y = 1.2
+		
+		if resistance >= 3:
+			if fire_power >=3:
+				$Sprite.texture = load("res://assets/ships/ship38.tres")
+			else:
+				$Sprite.texture = load("res://assets/ships/ship37.tres")
+		else:
+			if fire_power >= 3:
+				$Sprite.texture = load("res://assets/ships/ship36.tres")
+			else:
+				$Sprite.texture = load("res://assets/ships/ship35.tres")
--- scenes/Shipyard.gd
+++ scenes/Shipyard.gd
@@ -1,21 +1,81 @@
 extends Node2D
-var rand = 0
+
+onready var ship = preload("res://scenes/Ship.tscn")
+#variaveis para salvar em arquivo\/
+onready var building = false
+onready var currentBoat = []
+onready var currentFrame = 0
+
+export var speed = 1
+export var resistance = 1
+export var size = 1
+export var navegation_technologies = 1
+export var fire_power = 1
+export var improvement = false
+export var boat = [1,1]
 
 func _ready():
-	#script para teste de animação
-	randomize()
-	rand = int(rand_range(0,800))
+	if building:
+		buildShip(currentBoat[0],currentBoat[1],currentBoat[2],currentBoat[3],currentBoat[4],currentBoat[5],currentBoat[6])
+	buildShip(speed, resistance, size, navegation_technologies, fire_power, improvement, boat)#linha de teste
 
 func _physics_process(delta):
-	#script para teste de animação
-	if rand > 800:
-		if $AnimationPlayer.current_animation == "building" and $Ship.animation == "build" and $Ship.frame == 4:
-			$AnimationPlayer.play("ready")
-			$Cranes.frame = 0
-			$Cranes.playing = false
-		elif $AnimationPlayer.current_animation == "":
-			$AnimationPlayer.play("building")
-			$Ship.frame = 0
-			$Ship.play("build")
-			$Cranes.playing = true
-	rand+=1
+	if building:
+		currentFrame = $Ship.frame
+	if currentFrame == 7 and $Releasing.is_stopped():
+		release()
+
+func buildShip(speed, resistance, size, navegation_technologies, fire_power, improvement = false, boat = [1,1]):
+	var resistance_animation = "0"
+	var fire_power_animation = "0"
+	var upgrade_sequence = ""
+	building = true
+	currentBoat = [speed, resistance, size, navegation_technologies, fire_power, improvement, boat]
+	#define a orderm da animação do ultimo upgrade visual
+	if boat[0] < 3 and resistance >= 3 and boat[1] >= 3:
+		upgrade_sequence = "0"
+	elif boat[1] < 3 and fire_power >= 3 and boat[0] >= 3:
+		upgrade_sequence = "1"
+	#defina a animação correta para o barco em questão
+	if resistance >= 3:
+		resistance_animation = "1"
+	if fire_power >= 3:
+		fire_power_animation = "1"
+	if not improvement and resistance >= 3 and fire_power >= 3:
+		upgrade_sequence = "0"
+	
+	$Ship.play("build" + str(size) + resistance_animation + fire_power_animation + upgrade_sequence)
+	
+	if currentFrame == 0:
+		#define o frame correto da animação para caso de aprimoração
+		if improvement and (boat[0] >= 3 or boat[1] >= 3):
+			$Ship.frame = 5
+		elif improvement:
+			$Ship.frame = 4
+	else:
+		#define o frame correto que estava a animação caso o jogo for fechado
+		$Ship.frame = currentFrame
+	
+	$AnimationPlayer.play("building")
+	$Cranes.play("building")
+
+func release():
+	$AnimationPlayer.play("ready")
+	$Cranes.playing = false
+	$Releasing.start()
+
+func _on_Releasing_timeout():
+	$Ship.play("empty")
+	var new_ship = ship.instance()
+	new_ship.speed = currentBoat[0]
+	new_ship.resistance = currentBoat[1]
+	new_ship.size = currentBoat[2]
+	new_ship.navegation_technologies = currentBoat[3]
+	new_ship.fire_power = currentBoat[4]
+	new_ship.position = Vector2(position.x+1,position.y+42)
+	new_ship.define_sprite()
+	get_parent().add_child(new_ship)
+	new_ship.reposition()
+	building = false
+	currentBoat = []
+	currentFrame = 0
